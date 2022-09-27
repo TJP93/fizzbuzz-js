77b5a5cda2e50b140c8d6d81aa640a89e8ed7b8e
Tom Porter <tom.port@yahoo.co.uk>
2022-09-27 10:10:58 +0100

    11 working, begun on 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,20 +6,25 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (let i = 1; i < 101; i++) {
+    for (let i = 1; i < 255; i++) {
 
         let bang = "Bang"
         let fizz = "Fizz"
         let buzz = "Buzz"
-        
+        let bong = "Bong"
+        let fezz = "Fezz"
 
         let data = [];
+
+        if (i % 13 == 0) {data.unshift(fezz)}
 
         if (i % 3 == 0) {data.push(fizz)}
 
         if (i % 5 == 0) {data.push(buzz)}
 
         if (i % 7 == 0) {data.push(bang)}
+
+        if (i % 11 == 0) {data = [bong]}
 
         if (data.length == 0) 
         
